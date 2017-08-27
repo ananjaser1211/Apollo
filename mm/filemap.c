657cc4b6a939b34d65a24ef40b5954c8f8629f23
Linus Torvalds <torvalds@linux-foundation.org>
2017-08-27 16:25:09 -0700

    UPSTREAM: Avoid page waitqueue race leaving possible page locker waiting
    The "lock_page_killable()" function waits for exclusive access to the
    page lock bit using the WQ_FLAG_EXCLUSIVE bit in the waitqueue entry
    set.
    
    That means that if it gets woken up, other waiters may have been
    skipped.
    
    That, in turn, means that if it sees the page being unlocked, it *must*
    take that lock and return success, even if a lethal signal is also
    pending.
    
    So instead of checking for lethal signals first, we need to check for
    them after we've checked the actual bit that we were waiting for.  Even
    if that might then delay the killing of the process.
    
    This matches the order of the old "wait_on_bit_lock()" infrastructure
    that the page locking used to use (and is still used in a few other
    areas).
    
    Note that if we still return an error after having unsuccessfully tried
    to acquire the page lock, that is ok: that means that some other thread
    was able to get ahead of us and lock the page, and when that other
    thread then unlocks the page, the wakeup event will be repeated.  So any
    other pending waiters will now get properly woken up.
    
    Fixes: 62906027091f ("mm: add PageWaiters indicating tasks are waiting for a page bit")
    Cc: Nick Piggin <npiggin@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Davidlohr Bueso <dave@stgolabs.net>
    Cc: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    (cherry picked from commit a8b169afbf06a678437632709caac98e16f99263)
    
    Bug: 120776455
    Test: Build succeeds, device boots
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: I043dc3c37926e549b27ff08b68a601d3f067bbb4

--- mm/filemap.c
+++ mm/filemap.c
@@ -912,10 +912,6 @@ static inline int wait_on_page_bit_common(wait_queue_head_t *q,
 
 		if (likely(test_bit(bit_nr, &page->flags))) {
 			io_schedule();
-			if (unlikely(signal_pending_state(state, current))) {
-				ret = -EINTR;
-				break;
-			}
 		}
 
 		if (lock) {
@@ -924,6 +920,11 @@ static inline int wait_on_page_bit_common(wait_queue_head_t *q,
 		} else {
 			if (!test_bit(bit_nr, &page->flags))
 				break;
+		}
+
+		if (unlikely(signal_pending_state(state, current))) {
+			ret = -EINTR;
+			break;
 		}
 	}
 
