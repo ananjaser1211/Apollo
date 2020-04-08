5fd20b940e4d223f81da0ab6196276f8c938d601
Diep Quynh <remilia.1505@gmail.com>
2020-04-09 04:11:41 +0700

    devfreq_boost: Adapt to Exynos 9810
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- drivers/devfreq/exynos/exynos-devfreq.c
+++ drivers/devfreq/exynos/exynos-devfreq.c
@@ -27,6 +27,7 @@
 #include <linux/io.h>
 #include <linux/sched.h>
 #include <linux/exynos-wd.h>
+#include <linux/devfreq_boost.h>
 
 #include <soc/samsung/exynos-devfreq.h>
 #include <soc/samsung/tmu.h>
@@ -2220,6 +2221,9 @@ static int exynos_devfreq_probe(struct platform_device *pdev)
 		ret = -EINVAL;
 		goto err_devfreq;
 	}
+
+	if (data->devfreq_type == DEVFREQ_MIF)
+		devfreq_register_boost_device(DEVFREQ_EXYNOS_MIF, data->devfreq);
 
 	data->devfreq->min_freq = data->min_freq;
 	data->devfreq->max_freq = data->max_freq;
