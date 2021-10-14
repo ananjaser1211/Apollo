e58aa1e2815c13eea274ce8903e3be67723e0f9c
Lorenz Bauer <lmb@cloudflare.com>
2021-10-14 15:25:53 +0100

    bpf: Prevent increasing bpf_jit_limit above max
    [ Upstream commit fadb7ff1a6c2c565af56b4aacdd086b067eed440 ]
    
    Restrict bpf_jit_limit to the maximum supported by the arch's JIT.
    
    Signed-off-by: Lorenz Bauer <lmb@cloudflare.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20211014142554.53120-4-lmb@cloudflare.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -302,6 +302,7 @@ int bpf_jit_enable   __read_mostly = IS_BUILTIN(CONFIG_BPF_JIT_ALWAYS_ON);
 int bpf_jit_harden   __read_mostly;
 int bpf_jit_kallsyms __read_mostly;
 long bpf_jit_limit   __read_mostly;
+long bpf_jit_limit_max __read_mostly;
 
 static __always_inline void
 bpf_get_prog_addr_region(const struct bpf_prog *prog,
