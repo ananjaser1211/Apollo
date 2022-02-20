bc0dc0151cc9f5dd445a979bb86d38ca75b3c747
John Vincent <git@tenseventyseven.cf>
2022-02-20 15:05:22 +0800

    zram: Don't spam logs when writeback is finished
    It's not useful at all and just makes up for so much mess on our logs.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1467,7 +1467,7 @@ static void zram_comp_writeback(struct zram *zram)
 			break;
 	}
 	free_zwbs(zwbs);
-	pr_info("%s done", __func__);
+	pr_debug("%s done", __func__);
 }
 
 static int zram_wbd(void *p)
@@ -1509,7 +1509,7 @@ static int zram_wbd(void *p)
 				break;
 		}
 		zram->wbd_running = false;
-		pr_info("%s done", __func__);
+		pr_debug("%s done", __func__);
 	}
 	free_zwbs(zwbs);
 
