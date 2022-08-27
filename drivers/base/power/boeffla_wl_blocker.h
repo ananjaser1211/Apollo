2409f2cdd7a407c11fe5c375fd2d6b0a81f0aa2e
Anan Jaser <ananjaser@gmail.com>
2022-08-27 13:40:10 +0400

    boeffla_wl_blocker: restore display wakelock
    * possibly causing blank screens after idle

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				"16030000.decon_f;ssp_comm_wake_lock;GPSD;ssp_wake_lock;tsp_wakelock;umts_ipc0;umts_ipc1;mmc0_detect;wlan_txfl_wake;wlan_rx_wake;dwc3-otg;bluetooth_timer;BT_bt_wake;BT_host_wake;bbd_wake_lock;ssp_sensorhub_wake_lock;"
+#define LIST_WL_DEFAULT				"ssp_comm_wake_lock;GPSD;ssp_wake_lock;tsp_wakelock;umts_ipc0;umts_ipc1;mmc0_detect;wlan_txfl_wake;wlan_rx_wake;dwc3-otg;bluetooth_timer;BT_bt_wake;BT_host_wake;bbd_wake_lock;ssp_sensorhub_wake_lock;"
 
 #define LENGTH_LIST_WL				2048
 #define LENGTH_LIST_WL_DEFAULT		255
