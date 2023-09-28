9161c548c276a1e1d23d5513f526d90cfae88f18
Anan Jaser <ananjaser@gmail.com>
2023-09-28 14:23:37 +0400

    Revert "bpf: teach verifier to track stack depth"
    This reverts commit 78cbaaeb181b883085e59d90258c14530a52b8ee.

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -879,10 +879,6 @@ static int check_mem_access(struct bpf_verifier_env *env, int insn_idx, u32 regn
 			verbose("invalid stack off=%d size=%d\n", off, size);
 			return -EACCES;
 		}
-
-		if (env->prog->aux->stack_depth < -off)
-			env->prog->aux->stack_depth = -off;
-
 		if (t == BPF_WRITE) {
 			if (!env->allow_ptr_leaks &&
 			    state->stack_slot_type[MAX_BPF_STACK + off] == STACK_SPILL &&
@@ -999,9 +995,6 @@ static int check_stack_boundary(struct bpf_verifier_env *env, int regno,
 			regno, off, access_size);
 		return -EACCES;
 	}
-
-	if (env->prog->aux->stack_depth < -off)
-		env->prog->aux->stack_depth = -off;
 
 	if (meta && meta->raw_mode) {
 		meta->access_size = access_size;
@@ -3231,8 +3224,7 @@ process_bpf_exit:
 		insn_idx++;
 	}
 
-	verbose("processed %d insns, stack depth %d\n",
-		insn_processed, env->prog->aux->stack_depth);
+	verbose("processed %d insns\n", insn_processed);
 	return 0;
 }
 
