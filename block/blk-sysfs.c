66029b760486fe39c38ac804af9dec33c2248aa6
Anan Jaser <ananjaser@gmail.com>
2023-12-16 19:59:18 +0400

    block: update settings from N770F_13

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -87,11 +87,11 @@ queue_ra_store(struct request_queue *q, const char *page, size_t count)
 	unsigned long ra_kb;
 	ssize_t ret;
 	static const char temp[] = "temporary ";
-	
+
 	/* IOPP-ra-v2.0.4.4 */
 	if (strncmp(page, temp, sizeof(temp) - 1) != 0)
 		return count;
-	
+
 	page += sizeof(temp) - 1;
 
 	ret = queue_var_store(&ra_kb, page, count);
@@ -221,6 +221,7 @@ queue_max_sectors_store(struct request_queue *q, const char *page, size_t count)
 
 	spin_lock_irq(q->queue_lock);
 	q->limits.max_sectors = max_sectors_kb << 1;
+	q->backing_dev_info.io_pages = max_sectors_kb >> (PAGE_SHIFT - 10);
 	spin_unlock_irq(q->queue_lock);
 
 	return ret;
@@ -400,7 +401,7 @@ static struct queue_sysfs_entry queue_requests_entry = {
 };
 
 static struct queue_sysfs_entry queue_ra_entry = {
-	.attr = {.name = "read_ahead_kb", .mode = S_IRUGO | S_IWUSR },
+	.attr = {.name = "read_ahead_kb", .mode = 0644 },
 	.show = queue_ra_show,
 	.store = queue_ra_store,
 };
