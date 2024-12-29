bd46e6d5bb8ef8916f0d0d64b88b12432dd2e321
NeilBrown <neilb@suse.com>
2024-12-29 17:51:28 +0400

    cred: add get_cred_rcu()
    Sometimes we want to opportunistically get a
    ref to a cred in an rcu_read_lock protected section.
    get_task_cred() does this, and NFS does as similar thing
    with its own credential structures.
    To prepare for NFS converting to use 'struct cred' more
    uniformly, define get_cred_rcu(), and use it in
    get_task_cred().
    
    Signed-off-by: NeilBrown <neilb@suse.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- kernel/cred.c
+++ kernel/cred.c
@@ -299,7 +299,7 @@ const struct cred *get_task_cred(struct task_struct *task)
 	do {
 		cred = __task_cred((task));
 		BUG_ON(!cred);
-	} while (!atomic_inc_not_zero(&((struct cred *)cred)->usage));
+	} while (!get_cred_rcu(cred));
 #endif /*CONFIG_RKP_KDP*/
 	rcu_read_unlock();
 	return cred;
