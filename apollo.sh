fa56d9318be5c6781ec9ffcfe9ccfc66eefbf89d
Anan Jaser <ananjaser@gmail.com>
2025-06-10 16:40:36 +0400

    apollo: uprev to v7.0

--- apollo.sh
+++ apollo.sh
@@ -40,7 +40,7 @@ CR_DTB=$CR_DIR/arch/$CR_ARCH/boot/dtb.img
 # defconfig dir
 CR_DEFCONFIG=$CR_DIR/arch/$CR_ARCH/configs
 # Kernel Name and Version
-CR_VERSION=V6.7
+CR_VERSION=V7.0
 CR_NAME=Apollo
 # Thread count
 CR_JOBS=$(nproc --all)
