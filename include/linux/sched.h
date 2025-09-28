3189c2437f1554e5336c15a1c671105020782940
Anan Jaser <ananjaser@gmail.com>
2025-09-28 13:57:17 +0400

    susfs: upgrade to 1.5.7

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2211,7 +2211,6 @@ struct task_struct {
 /* CPU-specific state of this task */
 #ifdef CONFIG_KSU_SUSFS
 	u64 susfs_task_state;
-	u64 susfs_last_fake_mnt_id;
 #endif
 	struct thread_struct thread;
 /*
